bd5c8437454e85eb6fb8e54da59eda34a43d334a
Nortin Powers <a.nortin@gmail.com>
2023-06-06 21:02:28 +0300

    fix

--- src/test/java/by/tms/rest/template/utils/ObjectHandlerUtilsTest.java
+++ src/test/java/by/tms/rest/template/utils/ObjectHandlerUtilsTest.java
@@ -10,19 +10,19 @@ class ObjectHandlerUtilsTest {
     void getIgnoreProperties() {
         TestIncludeObject testIncludeObject = new TestIncludeObject(1L, "testName");
         TestObject testObject = new TestObject(1L, "name", 18, true, testIncludeObject);
-        assertArrayEquals(new String[]{}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        assertArrayEquals(new String[] {}, ObjectHandlerUtils.getIgnoreProperties(testObject));
         testIncludeObject = new TestIncludeObject(1L, null);
         testObject = new TestObject(1L, "name", 18, true, testIncludeObject);
-        assertArrayEquals(new String[]{}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        assertArrayEquals(new String[] {}, ObjectHandlerUtils.getIgnoreProperties(testObject));
         testIncludeObject = null;
         testObject = new TestObject(1L, "name", 18, true, testIncludeObject);
-        assertArrayEquals(new String[]{"includeObject"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        assertArrayEquals(new String[] {"includeObject"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
         testIncludeObject = new TestIncludeObject(1L, "testName");
         testObject = new TestObject(1L, null, null, true, testIncludeObject);
-        assertArrayEquals(new String[]{"name", "age"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        assertArrayEquals(new String[] {"name", "age"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
         testIncludeObject = new TestIncludeObject(1L, "testName");
         testObject = new TestObject(1L, "name", 18, null, testIncludeObject);
-        assertArrayEquals(new String[]{"trust"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        assertArrayEquals(new String[] {"trust"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
     }
 
     record TestObject(Long id, String name, Integer age, Boolean trust, TestIncludeObject includeObject) {
