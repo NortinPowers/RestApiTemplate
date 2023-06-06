15aadeb26ad4a69bd3b7882326a64470ea7b99c8
Nortin Powers <a.nortin@gmail.com>
2023-06-06 16:37:39 +0300

    added ObjectHandlerUtilsTest

---
+++ src/test/java/by/tms/rest/template/utils/ObjectHandlerUtilsTest.java
@@ -0,0 +1,36 @@
+package by.tms.rest.template.utils;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ObjectHandlerUtilsTest {
+
+    @Test
+    void getIgnoreProperties() {
+        TestIncludeObject testIncludeObject = new TestIncludeObject(1L, "testName");
+        TestObject testObject = new TestObject(1L, "name", 18, true, testIncludeObject);
+        assertArrayEquals(new String[]{}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        testIncludeObject = new TestIncludeObject(1L, null);
+        testObject = new TestObject(1L, "name", 18, true, testIncludeObject);
+        assertArrayEquals(new String[]{}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        testIncludeObject = null;
+        testObject = new TestObject(1L, "name", 18, true, testIncludeObject);
+        assertArrayEquals(new String[]{"includeObject"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        testIncludeObject = new TestIncludeObject(1L, "testName");
+        testObject = new TestObject(1L, null, null, true, testIncludeObject);
+        assertArrayEquals(new String[]{"name", "age"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+        testIncludeObject = new TestIncludeObject(1L, "testName");
+        testObject = new TestObject(1L, "name", 18, null, testIncludeObject);
+        assertArrayEquals(new String[]{"trust"}, ObjectHandlerUtils.getIgnoreProperties(testObject));
+    }
+
+    record TestObject(Long id, String name, Integer age, Boolean trust, TestIncludeObject includeObject) {
+
+    }
+
+    record TestIncludeObject(Long id, String name) {
+
+    }
+
+}
