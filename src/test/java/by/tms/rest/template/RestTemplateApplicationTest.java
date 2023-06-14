f73b902b4918f587c0fcbf9222fd5831a6a50265
Nortin Powers <a.nortin@gmail.com>
2023-06-14 22:47:43 +0300

    fix

--- src/test/java/by/tms/rest/template/RestTemplateApplicationTest.java
+++ src/test/java/by/tms/rest/template/RestTemplateApplicationTest.java
@@ -1,6 +1,5 @@
 package by.tms.rest.template;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import by.tms.rest.template.controller.CityController;
@@ -9,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestTemplateApplicationTests {
+class RestTemplateApplicationTest {
 
     @Autowired
     private CityController cityController;
